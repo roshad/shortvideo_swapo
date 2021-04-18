a81626b954b041fd1ed80bd3902ca28882ad2a24
roshad <roshad87a9@gmail.com>
2021-04-18 23:37:57 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// 引入组件样式
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import './static/css/iconfont.css'
+
+import utils from 'km-vue-utils'
+Vue.use(utils);
+Vue.prototype.$utils = utils;
+Vue.use(Vant);
+
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
